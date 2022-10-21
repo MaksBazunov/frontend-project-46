74a4b1833d717b6455cafcdec93d0bb03ca800e6
maksbazunov <maksbazunov@yandex.ru>
2022-10-21 17:11:46 +0300

    Create file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-
+import {  } from '/file/file1.json'
 const program = new Command();
  
 program.description('Compares two configuration files and shows a difference.');
 program.version('output the version number');
 program.option('-f, --format <type>','output format');
 program.arguments('<filepath1> <filepath2>')
+
 program.parse(process.argv);
---
+++ src/genDiff.js
@@ -0,0 +1,3 @@
+export const gendiff = (a ,b)=>{
+    console.log(b)
+}
