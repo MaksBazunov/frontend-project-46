d428700519c697d5d5c70b8d539d5d80cd2a5630
maksbazunov <maksbazunov@yandex.ru>
2022-10-21 08:51:40 +0300

    Creat option,arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,6 @@ const program = new Command();
  
 program.description('Compares two configuration files and shows a difference.');
 program.version('output the version number');
+program.option('-f, --format <type>','output format');
+program.arguments('<filepath1> <filepath2>')
 program.parse(process.argv);
