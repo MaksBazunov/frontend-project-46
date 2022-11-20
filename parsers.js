250e27957c5e904b689221d46133ae78fe781cc4
maksbazunov <maksbazunov@yandex.ru>
2022-11-20 17:23:07 +0300

    fix bug

--- parsers.js
+++ parsers.js
@@ -1,19 +1,19 @@
 import yaml from 'js-yaml';
 import path from 'path';
 
-const parse = (fileData,filePath) => {
+const parse = (fileData, filePath) => {
   const fileExt = path.extname(filePath).substring(1);
   if (fileExt === '') {
-    return JSON.parse(fileData)
+    return JSON.parse(fileData);
   }
   if (fileExt === 'json') {
-    return JSON.parse(fileData)
+    return JSON.parse(fileData);
   }
   if (fileExt === 'yaml') {
-    return yaml.load(fileData)
+    return yaml.load(fileData);
   }
   if (fileExt === 'yml') {
-    return yaml.load(fileData)
+    return yaml.load(fileData);
   }
   return 'Error: wrong file format';
 };
--- src/getDiff.js
+++ src/getDiff.js
@@ -1,26 +1,25 @@
 import path from 'path';
 import fs from 'fs';
-import  parse  from "../parsers.js";
-import  process from 'node:process';
-import genKey from '../genKey.js'
-import renderFormat from '../formatter/index.js'
+import process from 'node: process';
+import parse  from '../parsers.js';
+import genKey from '../genKey.js';
+import renderFormat from '../formatter/index.js';
 
 const getDiff = (filepath1, filepath2, format = 'stylish') => {
-const getFileData = (filePath)=>{
-  const absolutPathFile = path.resolve( process.cwd(), filePath);
-  return fs.readFileSync(absolutPathFile,'utf-8');
-}
+  const getFileData = (filePath) => {
+    const absolutPathFile = path.resolve(process.cwd(), filePath);
+    return fs.readFileSync(absolutPathFile,'utf-8');
+  };
 
-const fileData1 = getFileData(filepath1);
-const fileData2 = getFileData(filepath2);
+  const fileData1 = getFileData(filepath1);
+  const fileData2 = getFileData(filepath2);
 
-const parseFile1 = parse(fileData1,filepath1);
-const parseFile2 = parse(fileData2,filepath2);
+  const parseFile1 = parse(fileData1, filepath1);
+  const parseFile2 = parse(fileData2, filepath2);
 
-const diffKeys = genKey(parseFile1, parseFile2);
+  const diffKeys = genKey(parseFile1, parseFile2);
 
-  return renderFormat(diffKeys,format);
-
+  return renderFormat(diffKeys, format);
 };
 
 export default getDiff;
