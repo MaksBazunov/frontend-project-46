513ef76c4ddcaebb6797621b6e4300c8d1ee277c
maksbazunov <maksbazunov@yandex.ru>
2022-11-20 17:07:32 +0300

    fix bug

--- parsers.js
+++ parsers.js
@@ -5,16 +5,16 @@ const parse = (fileData,filePath) => {
   const fileExt = path.extname(filePath).substring(1);
   if (fileExt === '') {
     return JSON.parse(fileData)
-  };
+  }
   if (fileExt === 'json') {
     return JSON.parse(fileData)
-  };
+  }
   if (fileExt === 'yaml') {
     return yaml.load(fileData)
-  };
+  }
   if (fileExt === 'yml') {
     return yaml.load(fileData)
-  };
+  }
   return 'Error: wrong file format';
 };
 
