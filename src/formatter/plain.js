9bf7bfb6d76f75614988c71866c5157ed55b7d1c
maksbazunov <maksbazunov@yandex.ru>
2022-11-25 00:59:13 +0300

    fix bug rev2

--- src/formatter/plain.js
+++ src/formatter/plain.js
@@ -2,28 +2,28 @@ import _ from 'lodash';
 
 const stringify = (value) => {
   if (_.isObject(value)) {
-    return '[complex value]';
+      return '[complex value]';
   }
   if (_.isBoolean(value)) {
-    return value;
+      return value;
   }
   if (_.isNull(value)) {
-    return 'null';
+      return 'null';
   }
   return `'${value}'`;
 };
 
 const plain = (diffKeys, key = []) => _.compact(diffKeys.map((elem) => {
-  
+  const propKeys = `${[...key, elem.name].join('.')}`;
   switch (elem.state) {
     case 'nested':
       return plain(elem.children, [...key, elem.name]);
     case 'added':
-      return `Property '${[...key, elem.name].join('.')}' was added with value: ${stringify(elem.value)}`;
+      return `Property '${propKeys}' was added with value: ${stringify(elem.value)}`;
     case 'deleted':
-      return `Property '${[...key, elem.name].join('.')}' was removed`;
+      return `Property '${propKeys}' was removed`;
     case 'changed':
-      return `Property '${[...key, elem.name].join('.')}' was updated. From ${stringify(elem.previousValue)} to ${stringify(elem.currentValue)}`;
+      return `Property '${propKeys}' was updated. From ${stringify(elem.previousValue)} to ${stringify(elem.currentValue)}`;
     default:
       return '';
   }
