65d8f7e7efe2d129fb09e7a8f6baf131781c68c9
maksbazunov <maksbazunov@yandex.ru>
2022-11-25 09:02:30 +0300

    fix bug rev2

--- __test__/genDiff.test.js
+++ __test__/genDiff.test.js
@@ -12,22 +12,19 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8')
 
 test.each([['filepath1.json', 'filepath2.json', 'stylishFile.txt'],
   ['file1.yml', 'file2.yml', 'stylishFile.txt'],
-  ['file1.yml', 'filepath2.json', 'stylishFile.txt']])(
-    'diff format stylish', (filepath1, filepath2, expected) => {
+  ['file1.yml', 'filepath2.json', 'stylishFile.txt']])('diff format stylish', (filepath1, filepath2, expected) => {
       const actual = getDiff(getFixturePath(filepath1), getFixturePath(filepath2), 'stylish');
       expect(actual).toBe(readFile(expected));
 });
 test.each([['filepath1.json', 'filepath2.json', 'plainFile.txt'],
   ['file1.yml', 'file2.yml', 'plainFile.txt'],
-  ['file1.yml', 'filepath2.json', 'plainFile.txt']])(
-    'diff format plain', (filepath1, filepath2, expected) => {
+  ['file1.yml', 'filepath2.json', 'plainFile.txt']])('diff format plain', (filepath1, filepath2, expected) => {
   const actual = getDiff(getFixturePath(filepath1), getFixturePath(filepath2), 'plain');
   expect(actual).toBe(readFile(expected));
 });
 test.each([['filepath1.json', 'filepath2.json', 'jsonFile.txt'],
-  ['file1.yml','file2.yml','jsonFile.txt'],
-  ['file1.yml','filepath2.json','jsonFile.txt'],])(
-    'diff format json', (filepath1, filepath2, expected) => {
+  ['file1.yml', 'file2.yml', 'jsonFile.txt'],
+  ['file1.yml', 'filepath2.json', 'jsonFile.txt']])('diff format json', (filepath1, filepath2, expected) => {
   const actual = getDiff(getFixturePath(filepath1), getFixturePath(filepath2), 'json');
   expect(actual).toBe(readFile(expected));
 });
