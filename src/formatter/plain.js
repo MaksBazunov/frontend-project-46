8579f4eafa86c84c8964063a17983f350a1b2297
maksbazunov <maksbazunov@yandex.ru>
2022-11-25 07:12:55 +0300

    fix plain

--- src/formatter/plain.js
+++ src/formatter/plain.js
@@ -2,31 +2,28 @@ import _ from 'lodash';
 
 const stringify = (value) => {
   if (_.isObject(value)) {
-      return '[complex value]';
+    return '[complex value]';
   }
-  if (_.isBoolean(value)) {
-      return value;
-  }
-  if (_.isNumber(value)) {
+  if (_.isBoolean(value) || _.isNumber(value)) {
     return value;
   }
   if (_.isNull(value)) {
-      return 'null';
+    return 'null';
   }
   return `'${value}'`;
 };
 
 const plain = (diffKeys, key = []) => _.compact(diffKeys.map((elem) => {
-  const propKeys = `${[...key, elem.name].join('.')}`;
+  const keysProperty = `${[...key, elem.name].join('.')}`;
   switch (elem.state) {
     case 'nested':
       return plain(elem.children, [...key, elem.name]);
     case 'added':
-      return `Property '${propKeys}' was added with value: ${stringify(elem.value)}`;
+      return `Property '${keysProperty}' was added with value: ${stringify(elem.value)}`;
     case 'deleted':
-      return `Property '${propKeys}' was removed`;
+      return `Property '${keysProperty}' was removed`;
     case 'changed':
-      return `Property '${propKeys}' was updated. From ${stringify(elem.previousValue)} to ${stringify(elem.currentValue)}`;
+      return `Property '${keysProperty}' was updated. From ${stringify(elem.previousValue)} to ${stringify(elem.currentValue)}`;
     default:
       return '';
   }
