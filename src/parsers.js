bf23d10ccc3e4b6505407ed4c37607028e0f4959
maksbazunov <maksbazunov@yandex.ru>
2022-11-28 13:17:43 +0300

    creat parsers2

--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,9 @@
 import yaml from 'js-yaml';
 
 const parse = (fileData, fileFormat) => {
-  switch (fileFormat) {
-    case 'json':
-      return JSON.parse(fileData);
-    case 'yaml':
-      return yaml.load(fileData);
-    case 'yml':
-      return yaml.load(fileData);
-    default:
-      throw new Error('Error: wrong file format');
-  }
+  if (fileFormat === 'json') return JSON.parse(fileData);
+  if (fileFormat === 'yaml' || fileFormat === 'yml') return yaml.load(fileData);
+  throw new Error('Error: wrong file format');
 };
 
 export default parse;
