2cef49e4a66e6445ce55c232e7c5337b89a19476
maksbazunov <maksbazunov@yandex.ru>
2022-11-29 02:27:23 +0300

    fix bug2

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+
 const symbols = {
   unchanged: ' ',
   added: '+',
@@ -11,14 +12,13 @@ const setIndent = (depth, spaces = 2) => ' '.repeat(depth * indent - spaces);
 
 const stringify = (value, depth) => {
   if (!_.isObject(value)) return value;
-  
-  const arrayElem = Object.entries(value).map(([key, val]) => `${setIndent(depth)}  ${key}: ${stringify(val, depth + 1,)}`);
-  return ['{', ...arrayElem, `${setIndent(depth-1)}  }`, ].join('\n');
+  const arrayElem = Object.entries(value).map(([key, val]) => `${setIndent(depth)}  ${key}: ${stringify(val, depth + 1)}`);
+  return ['{', ...arrayElem, `${setIndent(depth - 1)}  }`, ] .join('\n');
 };
 
 const formatTree = (elem, depth) => {
   const keyValue = `${elem.name}: ${stringify(elem.value, depth + 1)}`;
-  const getTreeElem= (tree) => tree.map((element) => formatTree(element, depth + 1)).join('\n');
+  const getTreeElem = (tree) => tree.map((element) => formatTree(element, depth + 1)).join('\n');
 
   switch (elem.state) {
     case 'added':
@@ -28,7 +28,7 @@ const formatTree = (elem, depth) => {
     case 'unchanged':
       return `${setIndent(depth)}  ${keyValue}`;
     case 'changed':
-      return `${`${setIndent(depth)}- ${elem.name}: ${stringify(elem.previousValue,depth + 1,)}`}\n${
+      return `${`${setIndent(depth)}- ${elem.name}: ${stringify(elem.previousValue, depth + 1)}`}\n${
         `${setIndent(depth)}+ ${elem.name}: ${stringify(elem.currentValue, depth + 1)}`}`;
     case 'nested':
       return `${`${setIndent(depth)}${symbols[elem.state]} ${elem.name}`}: {\n${getTreeElem(elem.children)}\n  ${setIndent(depth)}}`;
