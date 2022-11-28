a07dc0ff10ca460f0604f7a18016e38e7b97b4a6
maksbazunov <maksbazunov@yandex.ru>
2022-11-29 02:21:58 +0300

    fix bug stylish

--- src/formatter/stylish.js
+++ src/formatter/stylish.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-
 const symbols = {
   unchanged: ' ',
   added: '+',
@@ -12,31 +11,27 @@ const setIndent = (depth, spaces = 2) => ' '.repeat(depth * indent - spaces);
 
 const stringify = (value, depth) => {
   if (!_.isObject(value)) return value;
-  return `{\n${Object.entries(value).map(([key, val]) => `${setIndent(depth)}  ${key}: ${stringify(
-    val,
-    depth + 1,
-  )
-  }`).join('\n')}\n${setIndent(depth - 1)}  }`;
+  
+  const arrayElem = Object.entries(value).map(([key, val]) => `${setIndent(depth)}  ${key}: ${stringify(val, depth + 1,)}`);
+  return ['{', ...arrayElem, `${setIndent(depth-1)}  }`, ].join('\n');
 };
 
 const formatTree = (elem, depth) => {
-  const dataUnchanged = `${setIndent(depth)}${symbols[elem.state]} ${elem.name}: ${stringify(elem.value, depth + 1)}`;
-  const dataDeleted = `${setIndent(depth)}${symbols.deleted} ${elem.name}: ${stringify(
-    elem.previousValue,
-    depth + 1,
-  )}`;
-  const dataAdded = `${setIndent(depth)}${symbols.added} ${elem.name}: ${stringify(elem.currentValue, depth + 1)}`;
-  const dataNested = `${setIndent(depth)}${symbols[elem.state]} ${elem.name}`;
+  const keyValue = `${elem.name}: ${stringify(elem.value, depth + 1)}`;
+  const getTreeElem= (tree) => tree.map((element) => formatTree(element, depth + 1)).join('\n');
+
   switch (elem.state) {
     case 'added':
+      return `${setIndent(depth)}+ ${keyValue}`;
     case 'deleted':
+      return `${setIndent(depth)}- ${keyValue}`;
     case 'unchanged':
-      return dataUnchanged;
+      return `${setIndent(depth)}  ${keyValue}`;
     case 'changed':
-      return `${dataDeleted}\n${dataAdded}`;
+      return `${`${setIndent(depth)}- ${elem.name}: ${stringify(elem.previousValue,depth + 1,)}`}\n${
+        `${setIndent(depth)}+ ${elem.name}: ${stringify(elem.currentValue, depth + 1)}`}`;
     case 'nested':
-      return `${dataNested}: {\n${elem.children
-        .map((element) => formatTree(element, depth + 1)).join('\n')}\n  ${setIndent(depth)}}`;
+      return `${`${setIndent(depth)}${symbols[elem.state]} ${elem.name}`}: {\n${getTreeElem(elem.children)}\n  ${setIndent(depth)}}`;
     default:
       throw new Error('Unknown state!');
   }
