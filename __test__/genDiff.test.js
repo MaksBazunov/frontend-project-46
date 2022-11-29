504acd9c3b108c7c7be5876e3294f1eeefc8ce90
maksbazunov <maksbazunov@yandex.ru>
2022-11-29 22:25:24 +0300

    fix bug test

--- __test__/genDiff.test.js
+++ __test__/genDiff.test.js
@@ -10,16 +10,27 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-const formats = ['yml', 'json'];
-const expectedJson = readFile('jsonFile.txt');
-const expectedPlain = readFile('plainFile.txt');
-const expectedStylish = readFile('stylishFile.txt');
+const extensions = [
+  ['json', 'json', undefined, 'stylishFile.txt'], 
+  ['yml', 'yml', undefined, 'stylishFile.txt'], 
+  ['json', 'yml', undefined, 'stylishFile.txt'],
 
-test.each(formats)('diff test', (format) => {
-  const filepath1 = getFixturePath(`file1.${format}`);
-  const filepath2 = getFixturePath(`file2.${format}`);
-  expect(getDiff(filepath1, filepath2, 'stylish')).toBe(expectedStylish);
-  expect(getDiff(filepath1, filepath2, 'plain')).toBe(expectedPlain);
-  expect(getDiff(filepath1, filepath2, 'json')).toBe(expectedJson);
-  expect(getDiff(filepath1, filepath2)).toBe(expectedStylish);
+  ['json', 'json', 'stylish', 'stylishFile.txt'], 
+  ['yml', 'yml', 'stylish', 'stylishFile.txt'], 
+  ['json', 'yml', 'stylish', 'stylishFile.txt'],
+
+  ['json', 'json', 'plain', 'plainFile.txt'], 
+  ['yml', 'yml', 'plain', 'plainFile.txt'], 
+  ['json', 'yml', 'plain', 'plainFile.txt'],
+
+  ['json', 'json', 'json', 'jsonFile.txt'], 
+  ['yml', 'yml', 'json', 'jsonFile.txt'], 
+  ['json', 'yml', 'json', 'jsonFile.txt']
+];
+
+test.each(extensions)('Diff test (%s, %s, %s)', (file1Extension, file2Extension, format, resultFile) => {
+  const filepath1 = getFixturePath(`file1.${file1Extension}`);
+  const filepath2 = getFixturePath(`file2.${file2Extension}`);
+  const result = readFile(resultFile);
+  expect(getDiff(filepath1, filepath2, format)).toBe(result);
 });
